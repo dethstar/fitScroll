fe419dc8f21e327ce81d4f307002a4bd50b39de7
Pedro <1110001@upv.edu.mx>
2013-09-19 22:55:41 -0500

    comments for other type of movement

--- winFit.py
+++ winFit.py
@@ -8,3 +8,15 @@ def girale(x,y):
 x,y = win32api.GetCursorPos()
 girale(x,y)
 
+# mouse move 'MOVE'        : 0x0001, 
+# Left button down 'LEFTDOWN'    : 0x0002, 
+# Left button up  'LEFTUP'      : 0x0004, 
+# Right button down  'RIGHTDOWN'   : 0x0008,
+# Right button up  'RIGHTUP'     : 0x0010,
+# middle button down  'MIDDLEDOWN'  : 0x0020, 
+# middle button up  'MIDDLEUP'    : 0x0040, 
+# x button down  'XDOWN'       : 0x0080, 
+# x button down  'XUP'         : 0x0100, 
+# wheel button rolled  'WHEEL'       : 0x0800, 
+# map To entire virtual Desktop  'VIRTUALDESK' : 0x4000, 
+# absolute move   'ABSOLUTE'    : 0x8000, 
