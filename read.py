6d907eebeddfa438a3321b27a1d8b9f5e58249d0
Pedro <1110001@upv.edu.mx>
2013-09-22 20:27:53 -0500

    comments time module used for tests

--- read.py
+++ read.py
@@ -1,30 +1,29 @@
 import platform
 import sys
 if platform.system()=="Linux":
-	print "Leenux"
 	device="/dev/tty"+sys.argv[1] #reads from command line
 	import linFit
 	scroll = linFit
 elif platform.system()=="Windows":
-	print "guindose"
 	import winFit
 	scroll = winFit
 	device="COM3"
 else:
-	print "not supported"
+	print("not supported")
+	sys.exit(0)
 
 del sys
 del platform
-import serial
-import time
+#import serial
+#import time
 #serial = serial.Serial(device,9600)
 while True:
-	x=raw_input()#serial.read()
+	x=input()#serial.read()
 	if x=="1":
-		time.sleep(2)
+#		time.sleep(2)
 		scroll.girale(1)
 	elif x=="-1":
-		time.sleep(2)
+#		time.sleep(2)
 		scroll.girale(-1)
 	else:
-		print x
+		print(x)
