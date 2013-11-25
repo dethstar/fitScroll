de74c7928d534f351778169ff057d481ac59d098
Pedro <1110001@upv.edu.mx>
2013-11-24 23:06:34 -0600

    fixs shit

--- read.py
+++ read.py
@@ -1,10 +1,7 @@
 import platform
 if platform.system()=="Linux":
-<<<<<<< HEAD
 	print "Leenux"
 	import sys
-=======
->>>>>>> 5257e0bb8b577c88b2af54220a394e5c593af91c
 	device="/dev/tty"+sys.argv[1] #reads from command line
 	del sys
 	import linFit
@@ -18,10 +15,8 @@ else:
 	sys.exit(0)
 
 del platform
-<<<<<<< HEAD
 import serial
 serial = serial.Serial(device,9600)
-import time #no va
 while True:
 	x=serial.read()#raw_input() 
 	if x=="1":
@@ -35,18 +30,3 @@ while True:
 	else:
 		print x
 	x=0 
-=======
-#import serial
-#import time
-#serial = serial.Serial(device,9600)
-while True:
-	x=input()#serial.read()
-	if x=="1":
-#		time.sleep(2)
-		scroll.girale(1)
-	elif x=="-1":
-#		time.sleep(2)
-		scroll.girale(-1)
-	else:
-		print(x)
->>>>>>> 5257e0bb8b577c88b2af54220a394e5c593af91c
